e9eccb7b62b939eef325324673cd9b2a190287c1
thibault.millet <thibault.millet@student-cs.fr>
2024-06-05 15:37:25 +0200

    meilleure présentation des films

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -1,15 +1,32 @@
-import '../../pages/Home/Home.css';
+import './Movie.css';
 export function Movie({ movie }) {
   return (
     <li>
-      <p>{movie.title}</p>
-      <p>{movie.release_date}</p>
       <p>
-        <img
-          src={`https://image.tmdb.org/t/p/w500` + movie.poster_path}
-          className="image"
-          alt={movie.title}
-        />
+        <div class="image-container">
+          <img
+            src={`https://image.tmdb.org/t/p/w500` + movie.poster_path}
+            class="hover-image"
+            alt={movie.title}
+          />
+          <div class="overlay">
+            <strong>{movie.title}</strong>
+            <br></br>
+            <br></br>
+            <span class="description">
+              {movie.overview.length > 200
+                ? movie.overview
+                    .slice(0, 200)
+                    .split(' ')
+                    .slice(0, -1)
+                    .join(' ') + '...'
+                : movie.overview}
+            </span>
+            <br></br>
+            <br></br>
+            <strong>{movie.release_date}</strong>{' '}
+          </div>
+        </div>
       </p>
     </li>
   );
