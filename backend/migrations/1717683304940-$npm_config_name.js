4a659c7febd3a7fd8e251932dab477d7cf0f2a04
Mouro Lucien <lucien.mouro@student-cs.fr>
2024-06-06 16:17:11 +0200

    migrations propre

--- backend/migrations/1717683304940-$npm_config_name.js
+++ backend/migrations/1717683304940-$npm_config_name.js
@@ -2,8 +2,8 @@ import typeorm from "typeorm";
 
 const { MigrationInterface, QueryRunner } = typeorm;
 
-export default class  $npmConfigName1717591948855 {
-    name = ' $npmConfigName1717591948855'
+export default class  $npmConfigName1717683304940 {
+    name = ' $npmConfigName1717683304940'
 
     async up(queryRunner) {
         await queryRunner.query(`
@@ -11,6 +11,36 @@ export default class  $npmConfigName1717591948855 {
                 "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
                 "genre_name" varchar NOT NULL,
                 CONSTRAINT "UQ_35a95dd11ad0db6e9684ca50df0" UNIQUE ("genre_name")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "movie" (
+                "id" integer PRIMARY KEY NOT NULL,
+                "title" varchar NOT NULL,
+                "release_date" varchar NOT NULL,
+                "original_language" varchar NOT NULL,
+                "overview" varchar NOT NULL,
+                "poster_path" varchar NOT NULL,
+                "popularity" integer NOT NULL,
+                "vote_average" integer NOT NULL,
+                CONSTRAINT "UQ_cb3bb4d61cf764dc035cbedd422" UNIQUE ("id")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "score" (
+                "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                "score" integer NOT NULL,
+                "moviesId" integer,
+                "usersId" integer
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "user" (
+                "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                "email" varchar NOT NULL,
+                "firstname" varchar NOT NULL,
+                "lastname" varchar NOT NULL,
+                CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")
             )
         `);
         await queryRunner.query(`
@@ -40,29 +70,29 @@ export default class  $npmConfigName1717591948855 {
             CREATE INDEX "IDX_1996ce31a9e067304ab168d671" ON "movie_genres_genre" ("genreId")
         `);
         await queryRunner.query(`
-            CREATE TABLE "temporary_movie" (
+            CREATE TABLE "temporary_score" (
                 "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                "title" varchar NOT NULL,
-                "release_date" varchar NOT NULL,
-                "original_language" varchar NOT NULL,
-                "overview" varchar NOT NULL,
-                "poster_path" varchar NOT NULL,
-                CONSTRAINT "UQ_a81090ad0ceb645f30f9399c347" UNIQUE ("title")
+                "score" integer NOT NULL,
+                "moviesId" integer,
+                "usersId" integer,
+                CONSTRAINT "FK_a75bf7ebbde9d19819d00240731" FOREIGN KEY ("moviesId") REFERENCES "movie" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+                CONSTRAINT "FK_f809ba06f8a61836a840bf00820" FOREIGN KEY ("usersId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
             )
         `);
         await queryRunner.query(`
-            INSERT INTO "temporary_movie"("id", "title", "release_date")
+            INSERT INTO "temporary_score"("id", "score", "moviesId", "usersId")
             SELECT "id",
-                "title",
-                "release_date"
-            FROM "movie"
+                "score",
+                "moviesId",
+                "usersId"
+            FROM "score"
         `);
         await queryRunner.query(`
-            DROP TABLE "movie"
+            DROP TABLE "score"
         `);
         await queryRunner.query(`
-            ALTER TABLE "temporary_movie"
-                RENAME TO "movie"
+            ALTER TABLE "temporary_score"
+                RENAME TO "score"
         `);
         await queryRunner.query(`
             DROP INDEX "IDX_dff457c114a6294863814818b0"
@@ -200,26 +230,27 @@ export default class  $npmConfigName1717591948855 {
             CREATE INDEX "IDX_dff457c114a6294863814818b0" ON "genre_movies_movie" ("genreId")
         `);
         await queryRunner.query(`
-            ALTER TABLE "movie"
-                RENAME TO "temporary_movie"
+            ALTER TABLE "score"
+                RENAME TO "temporary_score"
         `);
         await queryRunner.query(`
-            CREATE TABLE "movie" (
+            CREATE TABLE "score" (
                 "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                "title" varchar NOT NULL,
-                "release_date" varchar NOT NULL,
-                CONSTRAINT "UQ_a81090ad0ceb645f30f9399c347" UNIQUE ("title")
+                "score" integer NOT NULL,
+                "moviesId" integer,
+                "usersId" integer
             )
         `);
         await queryRunner.query(`
-            INSERT INTO "movie"("id", "title", "release_date")
+            INSERT INTO "score"("id", "score", "moviesId", "usersId")
             SELECT "id",
-                "title",
-                "release_date"
-            FROM "temporary_movie"
+                "score",
+                "moviesId",
+                "usersId"
+            FROM "temporary_score"
         `);
         await queryRunner.query(`
-            DROP TABLE "temporary_movie"
+            DROP TABLE "temporary_score"
         `);
         await queryRunner.query(`
             DROP INDEX "IDX_1996ce31a9e067304ab168d671"
@@ -238,6 +269,15 @@ export default class  $npmConfigName1717591948855 {
         `);
         await queryRunner.query(`
             DROP TABLE "genre_movies_movie"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "user"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "score"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "movie"
         `);
         await queryRunner.query(`
             DROP TABLE "genre"
