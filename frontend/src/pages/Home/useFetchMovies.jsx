4335776494d4518838d4f5394c274006fe1b0417
Mouro Lucien <lucien.mouro@student-cs.fr>
2024-06-06 14:44:15 +0200

    algo

--- frontend/src/pages/Home/useFetchMovies.jsx
+++ frontend/src/pages/Home/useFetchMovies.jsx
@@ -5,25 +5,12 @@ import axios from 'axios';
 export function useFetchMovies() {
   const [movies, setMovies] = useState([]);
   const fetchMovies = (movie = '', page = 1, sortBy = null) => {
-    const req =
-      movie.length > 0
-        ? `https://api.themoviedb.org/3/search/movie?query=${movie}&page=${page}`
-        : sortBy
-        ? `https://api.themoviedb.org/3/discover/movie?sort_by=${sortBy}&page=${page}`
-        : `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`;
     axios
-      .get(req, {
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo',
-          accept: 'application/json',
-        },
-      })
+      .get('http://localhost:8000/movies')
       .then((response) => {
         // Do something if call succeeded
-        console.log(req);
         console.log(response);
-        const films = response.data.results;
+        const films = response.data.movies;
         setMovies(films);
       })
       .catch((error) => {
